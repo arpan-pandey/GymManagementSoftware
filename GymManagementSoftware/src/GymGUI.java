92b19845e1ed74ed9d63e1bb20cad6012c16a523
arpan-pandey <p.arpanpandey@gmail.com>
2025-03-16 12:43:37 +0545

    removed test println

--- GymManagementSoftware/src/GymGUI.java
+++ GymManagementSoftware/src/GymGUI.java
@@ -1656,8 +1656,7 @@ public class GymGUI{
 	                                	
 		                            	// casting member to RegularMember, to upgradePlan
 		                            	RegularMember regularMember = (RegularMember) member;
-		                            	
-		                            	System.out.println(1);
+
 		                            	regularMember.upgradePlan(plan);
 	                                }
 	                            }
